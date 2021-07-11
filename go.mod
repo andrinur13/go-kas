a6381852c982749135f7ea0176c1e0aaa2d14616
andrinur13 <andribis13@gmail.com>
2021-07-11 21:44:00 +0700

    connect db

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module go-kas
 
 go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.11 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func loadENV() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("error load env file")
+	}
+}
+
 func main() {
+	loadENV()
+
+	fmt.Println("username", os.Getenv("DB_USER"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Ada error")
+	}
 
 }
